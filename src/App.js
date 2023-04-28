c11fbbbc71d469f3688eb0635c4285bd0b1ede6f
mohsenghorbanipour <mohsenghorban45@gmail.com@gmail.com>
2023-04-28 11:26:15 +0330

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles/App.css";
 import VideoCarousel from "./components/VideoCarousel";
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <VideoCarousel />
-    </div>
+    <Router>
+      <div className="App">
+          <Route exact path="/" component={VideoCarousel}></Route>
+      </div>
+    </Router>
   );
 }
 
